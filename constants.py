7a3616b5bb292cf513bedab017754effea6c7a9f
Xiaohan Tian <xiaohan.tian@stu.ocsb.ca>
2025-03-04 23:18:45 +0000

    pizza_cost_update.py

---
+++ constants.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# Created by: Xiaohan tian
+# Created on: Mar 3, 2025
+# This program contains the constants used to calculate the cost of pizza
+
+HST = 0.13
+LABOUR_COST = 2.00
+RENTAL_COST = 2.25
+INGRED_COST = 1.5
---
+++ pizza_cost.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# Created by: Xiaohan Tian
+# Created on: Mar 3, 2025
+# This program is to calculate the pizza's prize
+# including tax
+import constants
+
+
+def main():
+    # input of from the user
+    diameter = int(input("Enter the diameter of the pizza (inches): "))
+
+    # the process
+    subtotal = (
+        constants.LABOUR_COST + constants.INGRED_COST * diameter + constants.RENTAL_COST
+    )
+    tax = subtotal * constants.HST
+    total = subtotal + tax
+
+    # output
+    print("")
+    print("the total cost of the pizza is = ${:,.2f}".format(total))
+
+if __name__ == "__main__":
+    main()
